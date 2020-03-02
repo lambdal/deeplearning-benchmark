d566a43d2a929a9479732052a0a20810410e420a
chuanli11 <cl.chuanli@gmail.com>
2020-03-01 18:45:33 -0800

    Fixed a bug in envolking PyTorch maskrcnn

--- scripts/run_benchmark_pytorch.sh
+++ scripts/run_benchmark_pytorch.sh
@@ -9,29 +9,29 @@ echo ${NUM_GPU}
 
 
 declare -A TASKS=(
-    # [PyTorch_SSD_FP32]=benchmark_pytorch_ssd
-    # [PyTorch_SSD_AMP]=benchmark_pytorch_ssd
-    # [PyTorch_resnet50_FP32]=benchmark_pytorch_resnet50
-    # [PyTorch_resnet50_FP16]=benchmark_pytorch_resnet50
-    # [PyTorch_resnet50_AMP]=benchmark_pytorch_resnet50
-    # [PyTorch_maskrcnn_FP32]=benchmark_pytorch_maskrcnn
-    # [PyTorch_maskrcnn_FP16]=benchmark_pytorch_maskrcnn
-    # [PyTorch_gnmt_FP32]=benchmark_pytorch_gnmt
-    # [PyTorch_gnmt_FP16]=benchmark_pytorch_gnmt
-    # [PyTorch_ncf_FP32]=benchmark_pytorch_ncf
-    # [PyTorch_ncf_FP16]=benchmark_pytorch_ncf
-    # [PyTorch_transformerxlbase_FP32]=benchmark_pytorch_transformerxl
-    # [PyTorch_transformerxlbase_FP16]=benchmark_pytorch_transformerxl
-    # [PyTorch_transformerxllarge_FP32]=benchmark_pytorch_transformerxl
-    # [PyTorch_transformerxllarge_FP16]=benchmark_pytorch_transformerxl
+    [PyTorch_SSD_FP32]=benchmark_pytorch_ssd
+    [PyTorch_SSD_AMP]=benchmark_pytorch_ssd
+    [PyTorch_resnet50_FP32]=benchmark_pytorch_resnet50
+    [PyTorch_resnet50_FP16]=benchmark_pytorch_resnet50
+    [PyTorch_resnet50_AMP]=benchmark_pytorch_resnet50
+    [PyTorch_maskrcnn_FP32]=benchmark_pytorch_maskrcnn
+    [PyTorch_maskrcnn_FP16]=benchmark_pytorch_maskrcnn
+    [PyTorch_gnmt_FP32]=benchmark_pytorch_gnmt
+    [PyTorch_gnmt_FP16]=benchmark_pytorch_gnmt
+    [PyTorch_ncf_FP32]=benchmark_pytorch_ncf
+    [PyTorch_ncf_FP16]=benchmark_pytorch_ncf
+    [PyTorch_transformerxlbase_FP32]=benchmark_pytorch_transformerxl
+    [PyTorch_transformerxlbase_FP16]=benchmark_pytorch_transformerxl
+    [PyTorch_transformerxllarge_FP32]=benchmark_pytorch_transformerxl
+    [PyTorch_transformerxllarge_FP16]=benchmark_pytorch_transformerxl
     [PyTorch_tacotron2_FP32]=benchmark_pytorch_tacotron2
     [PyTorch_tacotron2_FP16]=benchmark_pytorch_tacotron2
     [PyTorch_waveglow_FP32]=benchmark_pytorch_tacotron2
     [PyTorch_waveglow_FP16]=benchmark_pytorch_tacotron2
-    # [PyTorch_bert_large_squad_FP32]=benchmark_pytorch_bert_squad
-    # [PyTorch_bert_large_squad_FP16]=benchmark_pytorch_bert_squad
-    # [PyTorch_bert_base_squad_FP32]=benchmark_pytorch_bert_squad
-    # [PyTorch_bert_base_squad_FP16]=benchmark_pytorch_bert_squad    
+    [PyTorch_bert_large_squad_FP32]=benchmark_pytorch_bert_squad
+    [PyTorch_bert_large_squad_FP16]=benchmark_pytorch_bert_squad
+    [PyTorch_bert_base_squad_FP32]=benchmark_pytorch_bert_squad
+    [PyTorch_bert_base_squad_FP16]=benchmark_pytorch_bert_squad    
 )
 
 benchmark_pytorch_ssd() {
@@ -69,6 +69,8 @@ benchmark_pytorch_maskrcnn() {
     local command_para=$(sed 's/.*args //' <<<${!TASK_PARAMS})
 
     LOGFILE="/results/joblog.log"
+    GLOBAL_BATCH=`echo ${!TASK_PARAMS} | grep -oP '(?<=SOLVER.IMS_PER_BATCH )\w+'`
+
     python -m torch.distributed.launch --nproc_per_node=${NUM_GPU} tools/train_net.py \
     --skip-test \
     ${command_para} \
