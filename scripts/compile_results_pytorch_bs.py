ceb53161e1c062003efaead2e8fc9f10624fb591
chuanli11 <cl.chuanli@gmail.com>
2020-03-31 18:57:07 -0700

    rename the header

--- scripts/compile_results_pytorch_bs.py
+++ scripts/compile_results_pytorch_bs.py
@@ -18,29 +18,29 @@ list_system = [('8xV100', 8),
 
 # skip lines, offset at the end, need to be multiply by #gpu
 list_test = {
-             'PyTorch_SSD_FP32': (4, -1, 1),
-             'PyTorch_SSD_AMP': (4, -1, 1),
-             'PyTorch_resnet50_FP32': (7, -1, 1),
-             'PyTorch_resnet50_FP16': (9, -1, 1),
-             'PyTorch_resnet50_AMP': (9, -1, 1),
-             'PyTorch_maskrcnn_FP32': (4, -1, 0),
-             'PyTorch_maskrcnn_FP16': (4, -1, 0),
-             'PyTorch_gnmt_FP32': (4, -1, 1),
-             'PyTorch_gnmt_FP16': (4, -1, 1),
-             'PyTorch_ncf_FP32': (5, -1, 0),
-             'PyTorch_ncf_FP16': (5, -1, 0),
-             'PyTorch_transformerxlbase_FP32': (5, -1, 0),
-             'PyTorch_transformerxlbase_FP16': (5, -1, 0),
-             'PyTorch_transformerxllarge_FP32': (5, -1, 0),
-             'PyTorch_transformerxllarge_FP16': (5, -1, 0),
-             'PyTorch_tacotron2_FP32': (7, -1, 1),
-             'PyTorch_tacotron2_FP16': (7, -1, 1),
-             'PyTorch_waveglow_FP32': (8, -1, 1),
-             'PyTorch_waveglow_FP16': (8, -1, 1),
-             'PyTorch_bert_large_squad_FP32': (5, -1, 1),
-             'PyTorch_bert_large_squad_FP16': (5, -1, 1),
-             'PyTorch_bert_base_squad_FP32': (5, -1, 1),
-             'PyTorch_bert_base_squad_FP16': (5, -1, 1),
+             'PyTorch_SSD_FP32': (4, -1, 1, 'SSD_FP32'),
+             'PyTorch_SSD_AMP': (4, -1, 1, 'SSD_AMP'),
+             'PyTorch_resnet50_FP32': (7, -1, 1, 'resnet50_FP32'),
+             'PyTorch_resnet50_FP16': (9, -1, 1, 'resnet50_FP16'),
+             'PyTorch_resnet50_AMP': (9, -1, 1, 'resnet50_AMP'),
+             'PyTorch_maskrcnn_FP32': (4, -1, 0, 'maskrcnn_FP32'),
+             'PyTorch_maskrcnn_FP16': (4, -1, 0, 'maskrcnn_FP16'),
+             'PyTorch_gnmt_FP32': (4, -1, 1, 'gnmt_FP32'),
+             'PyTorch_gnmt_FP16': (4, -1, 1, 'gnmt_FP16'),
+             'PyTorch_ncf_FP32': (5, -1, 0, 'ncf_FP32'),
+             'PyTorch_ncf_FP16': (5, -1, 0, 'ncf_FP16'),
+             'PyTorch_transformerxlbase_FP32': (5, -1, 0, 'transformerxlbase_FP32'),
+             'PyTorch_transformerxlbase_FP16': (5, -1, 0, 'transformerxlbase_FP16'),
+             'PyTorch_transformerxllarge_FP32': (5, -1, 0, 'transformerxllarge_FP32'),
+             'PyTorch_transformerxllarge_FP16': (5, -1, 0, 'transformerxllarge_FP16'),
+             'PyTorch_tacotron2_FP32': (7, -1, 1, 'tacotron2_FP32'),
+             'PyTorch_tacotron2_FP16': (7, -1, 1, 'tacotron2_FP16'),
+             'PyTorch_waveglow_FP32': (8, -1, 1, 'waveglow_FP32'),
+             'PyTorch_waveglow_FP16': (8, -1, 1, 'waveglow_FP16'),
+             'PyTorch_bert_large_squad_FP32': (5, -1, 1, 'bert_large_squad_FP32'),
+             'PyTorch_bert_large_squad_FP16': (5, -1, 1, 'bert_large_squad_FP16'),
+             'PyTorch_bert_base_squad_FP32': (5, -1, 1, 'bert_base_squad_FP32'),
+             'PyTorch_bert_base_squad_FP16': (5, -1, 1, 'bert_base_squad_FP16'),
              }
 
 
@@ -56,16 +56,13 @@ def gather(system, num_gpu, df):
             line = lines[idx + value[0]].rstrip().split(" ")
             line = list(filter(lambda a: a != "", line))
             bs = int(line[value[1]][1:-1]) * (num_gpu if value[2] else 1)
-            print(system)
-            print(test_name)
-            print(bs)
             df.at[system, test_name] = bs
 
 
 def main():
     columns = []
     for test_name, value in sorted(list_test.iteritems()):
-        columns.append(test_name)
+        columns.append(value[3])
 
     df = pd.DataFrame(index=[i[0] for i in list_system], columns=columns)
     df = df.fillna(-1.0)
