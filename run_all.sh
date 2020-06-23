e89285f96695ef8758fbe24f0b7cd7fa39d71055
chuanli11 <cl.chuanli@gmail.com>
2020-06-23 16:42:19 -0700

    Add 2080Ti No NVlink x2, x4, x8

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#
+#docker run --gpus '"device=0"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2080Ti all"
+#
+#docker run --gpus '"device=0,1"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2x2080Ti all"
+#
+docker run --gpus '"device=0,1,2,3"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4x2080Ti ssd_fp32"
 
-docker run --gpus '"device=0,1"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xQuadroRTX8000 all"
+docker run --gpus all --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8x2080Ti ssd_fp32"
 
-docker run --gpus '"device=0,1,2,3"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xQuadroRTX8000 all"
-
-docker run --gpus all --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xQuadroRTX8000 all"
