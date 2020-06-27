0eeab1784b78c00fb1998725b40c4c62a4c84268
chuanli11 <cl.chuanli@gmail.com>
2020-06-27 10:18:38 -0700

    Add 2x, 4x, 8x QuadroRTX8000 trt

--- run_all.sh
+++ run_all.sh
@@ -3,9 +3,9 @@
 #docker run --gpus '"device=0"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh QuadroRTX8000NVlink_dualroot all"
 
 
-docker run --gpus '"device=0,1"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xQuadroRTX8000NVlink_trt2 all"
+docker run --gpus '"device=0,1"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xQuadroRTX8000_trt all"
 
-docker run --gpus '"device=0,1,2,3"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xQuadroRTX8000NVlink_trt2 all"
+docker run --gpus '"device=0,1,2,3"' --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xQuadroRTX8000_trt all"
 
-docker run --gpus all --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xQuadroRTX8000NVlink_trt2 all"
+docker run --gpus all --rm --shm-size=64g -v ~/data:/data -v $(pwd)"/scripts":/scripts -v $(pwd)"/results":/results nvcr.io/nvidia/pytorch:20.01-py3 /bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xQuadroRTX8000_trt all"
 
--- scripts/compile_results_pytorch.py
+++ scripts/compile_results_pytorch.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 path_result = 'results'
 
-list_system = ['2xQuadroRTX8000NVlink_trt', '4xQuadroRTX8000NVlink_trt', '8xQuadroRTX8000NVlink_trt'] 
+list_system = ['2xQuadroRTX8000_trt', '4xQuadroRTX8000_trt', '8xQuadroRTX8000_trt'] 
 list_test = {
             'PyTorch_SSD_FP32': ('SSD_FP32', "^.*Training performance =.*$", -2),
             'PyTorch_SSD_AMP': ('SSD_AMP', "^.*Training performance =.*$", -2),
