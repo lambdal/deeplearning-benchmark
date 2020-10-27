373fca25a6cc20d23ae7a7b1f1a57905000a6656
chuanli11 <cl.chuanli@gmail.com>
2020-10-27 11:40:31 -0700

    Test pytorch-20.10 build on local machine

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -3,12 +3,12 @@
 SYSTEM=${1:-"2080Ti"}
 TASK_NAME=${2:-"all"}
 
-pip install 'git+https://github.com/NVIDIA/dllogger'
+# pip install 'git+https://github.com/NVIDIA/dllogger'
 
-cp /scripts/patch/run_squad.py examples/bert
+# cp /scripts/patch/run_squad.py examples/bert
 cp /scripts/patch/multiproc.py examples/tacotron2
-cp /scripts/patch/run_squad.sh examples/bert/scripts
-cp /scripts/patch/box_encoder_cuda.cu examples/ssd/csrc
+# cp /scripts/patch/run_squad.sh examples/bert/scripts
+# cp /scripts/patch/box_encoder_cuda.cu examples/ssd/csrc
 
 
 if [[ "${TASK_NAME}" == *"ssd"* ]] || [ $TASK_NAME = "all" ] || [[ "${TASK_NAME}" == *"maskrcnn"* ]]; then
