6d2751dcd8cf1b0a9d1bed0167b4fddb254317d9
chuanli11 <cl.chuanli@gmail.com>
2022-10-20 22:34:48 -0700

    Tune waveglow for 4090

--- pytorch/scripts/config_v1/config_pytorch_24GB.sh
+++ pytorch/scripts/config_v1/config_pytorch_24GB.sh
@@ -280,7 +280,7 @@ PyTorch_waveglow_FP16_PARAMS=(
             --learning-rate           "0.0" 
             --epochs                  "2" 
             --segment-length          "8000"
-            --batch-size              "12" 
+            --batch-size              "18" 
             --weight-decay            "0" 
             --grad-clip-thresh        "65504"
             --log-file                "nvlog.json"
