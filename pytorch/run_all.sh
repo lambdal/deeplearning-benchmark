65a7e4184a3a164d4a615a7319f0edabaf60aa46
chuanli11 <cl.chuanli@gmail.com>
2022-11-30 08:41:53 -0800

    Update 1x, 2x H100 PCIe4

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+docker run \
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh H100_80GB_PCIe_v1 all 3000"
+
+docker run \
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xH100_80GB_PCIe_v1 all 3000"
+
+
 #docker run \
 #	--rm --shm-size=1024g \
 #	--gpus all \
@@ -8,7 +29,8 @@
 #	-v $(pwd)"/scripts":/scripts \
 #	-v $(pwd)"/results":/results \
 #	nvcr.io/nvidia/${NAME_NGC} \
-#	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh H100_80GB_PCIe_v1 all 3000"
+#	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xH100_80GB_PCIe_v1 all 3000"
+#
 #
 #docker run \
 #	--rm --shm-size=1024g \
@@ -18,26 +40,4 @@
 #	-v $(pwd)"/scripts":/scripts \
 #	-v $(pwd)"/results":/results \
 #	nvcr.io/nvidia/${NAME_NGC} \
-#	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xH100_80GB_PCIe_v1 all 3000"
-
-
-docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xH100_80GB_PCIe_v1 all 3000"
-
-
-docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xH100_80GB_PCIe_v1 all 3000"
+#	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xH100_80GB_PCIe_v1 all 3000"
