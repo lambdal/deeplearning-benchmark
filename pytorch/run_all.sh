e7fc97854b8f58bb7678f3d2d13d412952aa11e5
chuanli11 <cl.chuanli@gmail.com>
2022-12-16 05:26:11 +0000

    Add LambdaCloud V100 results

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -8,7 +8,7 @@ docker run \
 	-v $(pwd)"/scripts":/scripts \
 	-v $(pwd)"/results":/results \
 	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh H100_80GB_PCIe_v1 all 3000"
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_V100_16GB_v1 all 3000"
 
 docker run \
 	--rm --shm-size=1024g \
@@ -18,24 +18,24 @@ docker run \
 	-v $(pwd)"/scripts":/scripts \
 	-v $(pwd)"/results":/results \
 	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xH100_80GB_PCIe_v1 all 3000"
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_2xV100_16GB_v1 all 3000"
 
-#docker run \
-#	--rm --shm-size=1024g \
-#	--gpus all \
-#	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-#	-v ~/data:/data \
-#	-v $(pwd)"/scripts":/scripts \
-#	-v $(pwd)"/results":/results \
-#	nvcr.io/nvidia/${NAME_NGC} \
-#
-#docker run \
-#	--rm --shm-size=1024g \
-#	--gpus all \
-#	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-#	-v ~/data:/data \
-#	-v $(pwd)"/scripts":/scripts \
-#	-v $(pwd)"/results":/results \
-#	nvcr.io/nvidia/${NAME_NGC} \
+docker run \
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_4xV100_16GB_v1 all 3000"
 
-#	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xH100_80GB_PCIe_v1 all 3000"
+docker run \
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_8xV100_16GB_v1 all 3000"
