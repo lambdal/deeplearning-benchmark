b65458f32451eafa87cf732c3d02650c6053404e
chuanli11 <cl.chuanli@gmail.com>
2023-01-12 15:02:11 -0800

    Add 1x 2x AdaA6000

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -28,7 +28,7 @@ docker run \
 	-v $(pwd)"/scripts":/scripts \
 	-v $(pwd)"/results":/results \
 	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_4xV100_16GB_v1 transformerxllarge 3000"
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xAdaA6000_v1 ncf 3000"
 
 docker run \
 	--rm --shm-size=1024g \
@@ -38,4 +38,4 @@ docker run \
 	-v $(pwd)"/scripts":/scripts \
 	-v $(pwd)"/results":/results \
 	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_8xV100_16GB_v1 transformerxllarge 3000"
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh AdaA6000_v1 ncf 3000"
