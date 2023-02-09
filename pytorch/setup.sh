84100274426ef4b7243ab018180334e64c1f33f1
Jordan Hewitt <1633901+src-r-r@users.noreply.github.com>
2023-02-09 14:58:55 -0800

    make sure git doesn't freak out if folder already exists.

--- pytorch/setup.sh
+++ pytorch/setup.sh
@@ -10,9 +10,11 @@ docker pull nvcr.io/nvidia/${NAME_NGC}
 echo "---------------------------------------------------"
 echo "Clone Repos: "
 echo "---------------------------------------------------"
-git clone https://github.com/LambdaLabsML/DeepLearningExamples.git && \
+export TARGET_DIR=DeepLearningExamples
+git -C "$TARGET_DIR" pull || git clone https://github.com/LambdaLabsML/${TARGET_DIR}.git "$TARGET_DIR" && \
 cd DeepLearningExamples && \
 git checkout lambda/benchmark && \
 cd ..
-git clone https://github.com/lambdal/deeplearning-benchmark.git && \
+export TARGET_DIR=deeplearning-benchmark
+git -C "$TARGET_DIR" pull || git clone https://github.com/lambdal/${TARGET_DIR}.git "$TARGET_DIR" && \
 cd deeplearning-benchmark/pytorch
