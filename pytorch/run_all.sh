4dc233109d7efd817d6c5117acabfbbf43f05961
chuanli11 <cl.chuanli@gmail.com>
2023-02-15 20:58:53 +0000

    update lambdacloud_a10 SSD

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -9,34 +9,5 @@ docker run \
 	-v $(pwd)"/scripts":/scripts \
 	-v $(pwd)"/results":/results \
 	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh H100_80GB_SXM5_v1 all 3000"
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_A10_v1 ssd 3000"
 
-docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 2xH100_80GB_SXM5_v1 all 3000"
-
-docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xH100_80GB_SXM5_v1 all 3000"
-
-docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xH100_80GB_SXM5_v1 all 3000"
