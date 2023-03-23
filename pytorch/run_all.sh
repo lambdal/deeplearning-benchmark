fc6c1e012a21704c656ea348c685ee1200c0e605
chuanli11 <cl.chuanli@gmail.com>
2023-03-23 04:27:44 +0000

    Add 4x, 8x AdaA6000

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+docker run \
+--rm --shm-size=1024g \
+--gpus all \
+-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+-v ~/data:/data \
+-v $(pwd)"/scripts":/scripts \
+-v $(pwd)"/results":/results \
+nvcr.io/nvidia/${NAME_NGC} \
+/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 4xAdaA6000_v1 all 3000"
 
 docker run \
-	--rm --shm-size=1024g \
-	--gpus all \
-	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
-	-v ~/data:/data \
-	-v $(pwd)"/scripts":/scripts \
-	-v $(pwd)"/results":/results \
-	nvcr.io/nvidia/${NAME_NGC} \
-	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh LambdaCloud_A10_v1 ssd 3000"
-
+--rm --shm-size=1024g \
+--gpus all \
+-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+-v ~/data:/data \
+-v $(pwd)"/scripts":/scripts \
+-v $(pwd)"/results":/results \
+nvcr.io/nvidia/${NAME_NGC} \
+/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh 8xAdaA6000_v1 all 3000"
