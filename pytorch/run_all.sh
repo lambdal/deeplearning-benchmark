4e11f6891ada05d1b317309d9b72c6072aa166d1
chuanli11 <cl.chuanli@gmail.com>
2024-07-24 14:44:34 +0000

    Add DellTexas

--- pytorch/run_all.sh
+++ pytorch/run_all.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
+
+# 1xGPU
 docker run \
---rm --shm-size=1024g \
---gpus all \
--v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
--v ~/data:/data \
--v $(pwd)"/scripts":/scripts \
--v $(pwd)"/results":/results \
-nvcr.io/nvidia/${NAME_NGC} \
-/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh RAIDZ1_H100_80GB_SXM5_v1 all 3000"
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh DellTexas_H100_80GB_SXM5_v1 all 3000"
 
+# 2xGPU
 docker run \
---rm --shm-size=1024g \
---gpus all \
--v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
--v ~/data:/data \
--v $(pwd)"/scripts":/scripts \
--v $(pwd)"/results":/results \
-nvcr.io/nvidia/${NAME_NGC} \
-/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh RAIDZ1_2xH100_80GB_SXM5_v1 all 3000"
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh DellTexas_2xH100_80GB_SXM5_v1 all 3000"
 
-
+# 4xGPU
 docker run \
---rm --shm-size=1024g \
---gpus all \
--v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
--v ~/data:/data \
--v $(pwd)"/scripts":/scripts \
--v $(pwd)"/results":/results \
-nvcr.io/nvidia/${NAME_NGC} \
-/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh RAIDZ1_4xH100_80GB_SXM5_v1 all 3000"
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh DellTexas_4xH100_80GB_SXM5_v1 all 3000"
 
-
-
+# 4xGPU
 docker run \
---rm --shm-size=1024g \
---gpus all \
--v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
--v ~/data:/data \
--v $(pwd)"/scripts":/scripts \
--v $(pwd)"/results":/results \
-nvcr.io/nvidia/${NAME_NGC} \
-/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh RAIDZ1_8xH100_80GB_SXM5_v1 all 3000"
-
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh DellTexas_8xH100_80GB_SXM5_v1 all 3000"
 
