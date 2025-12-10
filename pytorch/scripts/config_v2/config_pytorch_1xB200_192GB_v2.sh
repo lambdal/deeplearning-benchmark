200af3b7ce8ee1f55c7ed6eaee0df2cafe2d7886
dkong.yb200 <deqiankong@ucla.edu>
2025-12-10 06:50:46 +0000

    add b200

---
+++ pytorch/scripts/config_v2/config_pytorch_1xB200_192GB_v2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source config_v2/config_pytorch_192GB.sh
+
+declare -A BATCH_SIZE_FIX=(
+)
+
+declare -A tacotron2_DATA_FIX=(
+    [PyTorch_tacotron2_FP32]="filelists/ljs_audio_text_train_subset_1250_filelist.txt"
+    [PyTorch_tacotron2_FP16]="filelists/ljs_audio_text_train_subset_1250_filelist.txt"
+)
+
+source config_v2/fix.sh
